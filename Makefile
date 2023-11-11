e17bdde076eb507f9ef346b7866bd824774d41a3
Frederic MAMATH <fredericmamath@gmail.com>
2023-11-11 18:42:42 +0100

    :tada: Starts project

---
+++ Makefile
@@ -0,0 +1,5 @@
+make deploy:
+	forge deploy
+
+make dev:
+	forge tunnel
---
+++ src/index.jsx
@@ -0,0 +1,39 @@
+import ForgeUI, {
+  render,
+  Fragment,
+  Text,
+  IssuePanel,
+  useProductContext,
+  useState,
+} from "@forge/ui";
+import api, { route } from "@forge/api";
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+
+  const data = await res.json();
+
+  return data.comments;
+};
+
+const App = () => {
+  const context = useProductContext();
+
+  const [comments] = useState(
+    async () => await fetchCommentsForIssue(context.platformContext.issueKey)
+  );
+
+  return (
+    <Fragment>
+      <Text>Number of comments on this issue: {comments.length}</Text>
+    </Fragment>
+  );
+};
+
+export const run = render(
+  <IssuePanel>
+    <App />
+  </IssuePanel>
+);
